6f8f131c0ccdfd7978169854c8bd5319576337bf
Jay <jaypaulaying5@gmail.com>
2015-01-22 10:18:24 +0800

    created initial user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(username: "wild_dust", email: "jaypaulaying5@gmail.com", password: "wilddust_2015")
+user = User.create(username: "wild_dust", email: "jaypaulaying5@gmail.com", password: "wilddust_2015")
