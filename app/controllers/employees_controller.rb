9274218baddb0b4e1d75bad3af150b78b058b50a
Jay <jaypaulaying5@gmail.com>
2015-05-30 21:57:41 +0800

    fix postres error

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,26 +1,26 @@
 class EmployeesController < ApplicationController
   before_action :authenticate_user!
 
-	def index
-		@employees = Employee.all
-	end
-
-	def show
-		@employee = Employee.find(params[:id])
-    @sewed_date = params[:sew_date].blank? ? Time.now : params[:sew_date].to_date
-    @sewed_clothings_on_date = @employee.sewed_clothings.sewed_on_date(@sewed_date.strftime("%Y-%m-%d"))
-    @sewed_clothing = @employee.sewed_clothings.new    
+  def index
+    @employees = Employee.all
   end
 
-	def new
-		@employee = Employee.new
-	end
-def
-	 create
-	  @employee = Employee.new(employee_params)
-	  @employee.save
-	  redirect_to @employee
-	end
+  def show
+    @employee = Employee.find(params[:id])
+    @sewed_date = params[:sew_date].blank? ? Time.now : params[:sew_date].to_date
+    @sewed_clothings_on_date = @employee.sewed_clothings.sewed_on_date(@sewed_date.strftime("%Y-%m-%d"))
+    @sewed_clothing = @employee.sewed_clothings.new
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.save
+    redirect_to @employee
+  end
 
   def edit
     @employee =  Employee.find(params[:id])
@@ -38,7 +38,7 @@ def
   end
 
   private
-   def employee_params
+  def employee_params
     params.require(:employee).permit(:first_name, :last_name, :address, :contact_details, :birthday)
   end
 end
